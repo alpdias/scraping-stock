2d11c7b276725dd30017083bd1b4c352cb93c5a9
Paulo <al.pdias24@gmail.com>
2019-12-08 11:42:57 -0300

    format strings to view

--- src/request-stock-market.py
+++ src/request-stock-market.py
@@ -15,28 +15,34 @@ dataAtual = datetime.now()
 horas = dataAtual.hour
 
 # Função para verificar a conexão com o site
-def verificarConexao(): # Arrumar retorno
+def verificarConexao():
     try:
         url = (f'https://finance.yahoo.com/lookup')
         req = requests.get(url)
         fonteOnline = (req.status_code)
         if fonteOnline == 200:
-            print('\033[0;32mOnline\033[m')
+            fonte = ('\033[0;32mONLINE\033[m')
+            return fonte
         else:
-            print('\033[0;31mOffline\033[m')
+            fonte = ('\033[0;31mOFFLINE\033[m')
+            return fonte
     except:
-        print('\033[0;31m#ERRO#\033[m')
+        fonte = ('\033[0;31m#ERRO#\033[m')
+        return fonte
 
 
 # Função para mostra se a negocição da BM&FBOVESPA está aberta ou fechada
-def bovespaON(): # Arrumar retorno
+def bovespaON():
     try:
         if horas >= 10 and horas <= 17: # Adicionar diferença de dia para final de semana!
-            print('\033[0;32mBM&FBOVESPA ON\033[m') 
+            bovespa = ('\033[0;32mBM&FBOVESPA\033[m') 
+            return bovespa
         else:
-            print('\033[0;31mBM&FBOVESPA OFF\033[m') 
+            bovespa = ('\033[0;31mBM&FBOVESPA\033[m') 
+            return bovespa
     except:
-        print('\033[0;31m#ERRO#\033[m')
+        bovespa = ('\033[0;31m#ERRO#\033[m')
+        return bovespa
 
 
 # Funções de 'raspagem' da informação do site para obter os dados solicitados
@@ -57,7 +63,7 @@ def empresaUSD():
     nomeEmpresa = soup.find_all('div',{'class': 'D(ib) Mt(-5px) Mend(20px) Maw(56%)--tab768 Maw(52%) Ov(h) smartphone_Maw(85%) smartphone_Mend(0px)'})[0].find('h1').text
     valorEmpresa = soup.find_all('div',{'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
     print(f'Empresa: {nomeEmpresa}')
-    print(f'Preço atual {codigo}: {valorEmpresa} - Currency in USD')
+    print(f'Preço atual {codigo}: {valorEmpresa} - Currency in USD') # Mudar a formatação de valores para padrão pt-BR!
     print(f'Fonte: Yahoo Finance')
     print(f'{dataAtual}')
 
@@ -65,15 +71,20 @@ def empresaUSD():
 def indiceMercado():
     r = requests.get(f'https://finance.yahoo.com/quote/^{codigo}/')
     soup = bs4.BeautifulSoup(r.content, 'html.parser')
-    nomeIndice = soup.find_all('div',{'class': 'D(ib) Mt(-5px) Mend(20px) Maw(56%)--tab768 Maw(52%) Ov(h) smartphone_Maw(85%) smartphone_Mend(0px)'})[0].find('h1').text
+    nomeIndice = soup.find_all('div',{'class': 'D(ib) Mt(-5px) Mend(20px) Maw(56%)--tab768 Maw(52%) Ov(h) smartphone_Maw(85%) smartphone_Mend(0px)'})[0].find('h1').text.split()
     valorIndice = soup.find_all('div',{'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
-    print(f'Índice: {nomeIndice}') # Arrumar formato do nome aprensentado
-    print(f'Preço atual {codigo}: {valorIndice}')
+    print(f'Índice: {nomeIndice[2]}') 
+    print(f'Valor atual {codigo}: {valorIndice}') # Mudar a formatação de valores para padrão pt-BR!
     print(f'Fonte: Yahoo Finance')
     print(f'{dataAtual}')
 
 
 # Variável que recebe o código da empresa ou índice
+print('')
+print('-' * 40)
+print(' ' * 10 + 'REQUEST STOCK MARKET')
+print('-' * 40)
+print(' ' * 2 + f'{verificarConexao()}' + ' ' * 18 + f'{bovespaON()}')
 print('')
 codigo = str(input('Código da empresa ou índice: ')).upper() 
 
@@ -89,5 +100,6 @@ except IndexError:
             indiceMercado()
         except IndexError:
             print('\033[0;31mEmpresa/Índice não encontrada, tente novamente!\033[m')
-            
+print('-' * 40)
+
 print('')
