57bd84c9a5d4a428aa841186f5cee71bf1dc8e92
Paulo Alcântara <52184239+alpdias@users.noreply.github.com>
2020-11-30 15:28:38 -0300

    Update and rename cotacao-mercado.py to stock.py

--- src/stock.py
+++ src/stock.py
@@ -5,19 +5,26 @@ Criado em 12/2019
 @Autor: Paulo https://github.com/alpdias
 '''
 
-# Bibliotecas importadas
+# bibliotecas importadas
 import bs4
 import requests
+import datetime
+import locale
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-# Mostra o horário e data atual para verificações
+# mostra o horario e data atual para verificaçoes
 dataAtual = datetime.now()
 minutos = dataAtual.minute
 horasMinutos = ((dataAtual.hour * 60) + minutos) 
 
-# Função para verificar a conexão com o site
+
 def verificarConexao():
+    
+    """
+    -> Funçao para verificar a conexao com o site
+    """
+    
     try:
         url = (f'https://finance.yahoo.com/lookup')
         req = requests.get(url)
@@ -25,57 +32,73 @@ def verificarConexao():
         
         if fonteOnline == 200:
             fonte = ('\033[0;32mONLINE\033[m')
+            
             return fonte
         
         else:
             fonte = ('\033[0;31mOFFLINE\033[m')
+            
             return fonte
         
     except:
         fonte = ('\033[0;31mERRO\033[m')
+        
         return fonte
 
 
-# Função para mostra se a negocição da BM&FBOVESPA está aberta ou fechada
 def bovespaON():
-    import datetime
+    
+    """
+    -> Funçao para mostra se a negocição da BM&FBOVESPA esta aberta ou fechada
+    """
     
     diaSemana = datetime.datetime.today().weekday()
     
-    if diaSemana in [0, 1, 2, 3, 4]:
-        
+    if diaSemana in [0, 1, 2, 3, 4]:  
         try:
             if horasMinutos >= 600 and horasMinutos <= 1020:
                 bovespa = ('\033[0;32mABERTO\033[m') 
+                
                 return bovespa
             
             elif horasMinutos >= 1020 and horasMinutos <= 1080:
                 bovespa = ('\033[0;33mPRÉ-FECHAMENTO\033[m')
+                
                 return bovespa
             
             else:
                 bovespa = ('\033[0;31mFECHADO\033[m') 
+                
                 return bovespa
             
         except:
             bovespa = ('\033[0;31mERRO\033[m')
+            
             return bovespa
         
     else:
         bovespa = ('\033[0;31mFECHADO\033[m') 
+        
         return bovespa
 
 
-# Função para formatar os números de acordo com o padrão local selecionado
 def tratamento(n=0, formato=''):
-    import locale
     
+    """
+    -> Funçao para formatar os numeros de acordo com o padrao local selecionado
+    """
+
     locale.setlocale(locale.LC_MONETARY, formato)
+    
     return locale.currency(n, grouping=True)
 
 
-# Funções de 'raspagem' da informação do site para obter os dados solicitados
 def empresaBRL():
+    
+    """
+    -> Funçao para 'raspagem' da informação do site para obter os dados solicitados
+    """
+    
     r = requests.get(f'https://finance.yahoo.com/quote/{codigo}.SA/')
     soup = bs4.BeautifulSoup(r.content, 'html.parser')
     nomeEmpresa = soup.find_all('div', {'class': 'D(ib) Mt(-5px) Mend(20px) Maw(56%)--tab768 Maw(52%) Ov(h) smartphone_Maw(85%) smartphone_Mend(0px)'})[0].find('h1').text
@@ -89,6 +112,11 @@ def empresaBRL():
 
 
 def empresaUSD():
+          
+    """
+    -> Funçao para 'raspagem' da informação do site para obter os dados solicitados
+    """
+          
     r = requests.get(f'https://finance.yahoo.com/quote/{codigo}/')
     soup = bs4.BeautifulSoup(r.content, 'html.parser')
     nomeEmpresa = soup.find_all('div', {'class': 'D(ib) Mt(-5px) Mend(20px) Maw(56%)--tab768 Maw(52%) Ov(h) smartphone_Maw(85%) smartphone_Mend(0px)'})[0].find('h1').text
@@ -102,6 +130,11 @@ def empresaUSD():
 
 
 def indiceMercado():
+          
+    """
+    -> Funçao para 'raspagem' da informação do site para obter os dados solicitados
+    """
+          
     r = requests.get(f'https://finance.yahoo.com/quote/^{codigo}/')
     soup = bs4.BeautifulSoup(r.content, 'html.parser')
     nomeIndice = soup.find_all('div', {'class': 'D(ib) Mt(-5px) Mend(20px) Maw(56%)--tab768 Maw(52%) Ov(h) smartphone_Maw(85%) smartphone_Mend(0px)'})[0].find('h1').text.split()
@@ -115,13 +148,13 @@ def indiceMercado():
     print(f'{dataAtual}')
 
 
-# Exibição das informações requisitadas
 print('')
+          
 print(f'COTAÇÃO / MERCADO DE AÇÕES -- {verificarConexao()}')
 print(f'BM&FBOVESPA: {bovespaON()}')
+          
 print('')
 
-# Variável que recebe o código da empresa ou índice
 codigo = str(input('Código da empresa ou índice: ')).upper() 
 
 print('')
@@ -138,6 +171,6 @@ except IndexError:
             indiceMercado()
             
         except IndexError:
-            print('\033[0;31mERRO!\033[m Empresa/Índice não encontrada, tente novamente.')
+            print('\033[0;31mERRO!\033[m Empresa/Índice não encontrada, tente novamente!')
 
 print('')
